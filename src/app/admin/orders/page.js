89aeea5414c58d3e5473ba5ce2a4b3fe3a777148
tushar-454 <imtushar454@gmail.com>
2024-01-15 23:54:16 +0600

    get all order from admin and show on ui with all need info using table format

--- src/app/admin/orders/page.js
+++ src/app/admin/orders/page.js
@@ -1,9 +1,12 @@
+import AdminOrdersTableList from '@/components/shared/AdminOrdersTableList';
 import Title from '@/components/ui/Title';
 
 const Orders = () => {
   return (
     <div>
       <Title>সকল অর্ডার সমূহ</Title>
+      {/* all order table list  */}
+      <AdminOrdersTableList />
     </div>
   );
 };
---
+++ src/components/shared/AdminOrdersTableList.jsx
@@ -0,0 +1,84 @@
+import getApiData from '@/lib/getApiData';
+import Link from 'next/link';
+
+const AdminOrdersTableList = async () => {
+  const { data: allorders } = await getApiData('admin/getallorders');
+  return (
+    <div class='mr-5 relative overflow-x-auto shadow-md sm:rounded-lg'>
+      <table class='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+        <thead class='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+          <tr>
+            <th scope='col' class='px-6 py-3'>
+              কাস্টমার তথ্য
+            </th>
+            <th scope='col' class='px-6 py-3'>
+              ডেলিভারি ঠিকানা
+            </th>
+            <th scope='col' class='px-6 py-3'>
+              তারিখ
+            </th>
+            <th scope='col' class='px-6 py-3'>
+              উইশ বার্তা
+            </th>
+            <th scope='col' class='px-6 py-3'>
+              নির্দেশনা
+            </th>
+            <th scope='col' class='px-6 py-3'>
+              কেকের তথ্য
+            </th>
+            <th scope='col' class='px-6 py-3  whitespace-nowrap'>
+              মোট টাকা
+            </th>
+            <th scope='col' class='px-6 py-3 whitespace-nowrap'>
+              পেমেন্ট স্ট্যাটাস
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {allorders.map((order, index) => (
+            <tr
+              key={index}
+              class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'
+            >
+              <th
+                scope='row'
+                class='w-32 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
+              >
+                <p className='flex flex-col'>
+                  <span>{order.name}</span>
+                  <span>
+                    <a href={`tel:${order.mobile}`}>{order.mobile}</a>
+                  </span>
+                </p>
+              </th>
+              <td class='px-6 py-4'>{order.address}</td>
+              <td class='px-6 py-4 whitespace-nowrap'>{order.date}</td>
+              <td class='px-6 py-4'>{order.wishText}</td>
+              <td class='px-6 py-4'>{order.instruction}</td>
+              <td class='px-6 py-4'>
+                {order.orderItem.map((item, index) => (
+                  <div className='p-1 bg-supernova-100/30 mb-1'>
+                    <p key={index}>
+                      <Link
+                        href={`/cake/${item.cakeId}`}
+                        className='whitespace-nowrap'
+                      >
+                        কেকটি দেখুন
+                      </Link>
+                    </p>
+                    <p className='whitespace-nowrap'>ফ্লেভারঃ {item.flavour}</p>
+                    <p>পরিমানঃ {item.quentity}</p>
+                  </div>
+                ))}
+              </td>
+              <td class='px-6 py-4'>{order.totalMoney}</td>
+              <td class='px-6 py-4'>{order.paymentStatus}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AdminOrdersTableList;
