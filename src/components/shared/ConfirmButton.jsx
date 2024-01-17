eae43e3fb7339d61127bae31d6ad5cf500a7371f
tushar-454 <imtushar454@gmail.com>
2024-01-18 00:11:37 +0600

    show toast on confirm a order

--- src/components/shared/ConfirmButton.jsx
+++ src/components/shared/ConfirmButton.jsx
@@ -1,18 +1,38 @@
 'use client';
 
+import toast from 'react-hot-toast';
+
 const ConfirmButton = ({ id, orderStatus }) => {
   const handleApproveOrder = async () => {
-    fetch(`http://www.localhost:5000/api/v1/admin/updateorderinfo/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'Application/json',
-      },
-      body: JSON.stringify({ orderStatus: 'Confirm' }),
-    });
+    const res = await fetch(
+      `http://www.localhost:5000/api/v1/admin/updateorderinfo/${id}`,
+      {
+        next: {
+          revalidate: 1,
+        },
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'Application/json',
+        },
+        body: JSON.stringify({ orderStatus: 'confirm' }),
+      }
+    );
+    const { message } = await res.json();
+    if (message === 'success') {
+      toast.success('অর্ডারটি কনফার্ম করা হলো');
+    }
   };
   return (
     <td class='px-6 py-4'>
-      <button onClick={handleApproveOrder}>
+      <button
+        onClick={
+          orderStatus === 'pending'
+            ? handleApproveOrder
+            : () => {
+                toast.error('অর্ডারটি কনফার্ম করা হয়েছে');
+              }
+        }
+      >
         {orderStatus === 'pending' ? 'নিশ্চিত করুন' : 'নিশ্চিত'}
       </button>
     </td>
